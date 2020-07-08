52be5fb7794909da8cc1c26ca8abe1f128ec1e03
jgeneaguilar <jgeneaguilar@gmail.com>
2020-07-08 23:14:29 +0800

    Add scaffold to routes/views

--- video_server/routes.py
+++ video_server/routes.py
@@ -1,3 +1,23 @@
 def includeme(config):
     config.add_static_view("static", "static", cache_max_age=3600)
-    config.add_route("home", "/")
+
+    # User public routes
+    config.add_route("users", "/users")
+    config.add_route("create_user", "/users/create")
+    config.add_route("user", "/users/find/{username}")
+    config.add_route("authenticate", "/auth")
+
+    # User auth routes
+    config.add_route("change_password", "/users/{user_id}/password")
+    config.add_route("change_mobile_token", "/users/{user_id}/mobile_token")
+    config.add_route("delete_user", "/users/{user_id}/delete")
+
+    # Room public routes
+    config.add_route("get_room_info", "/rooms/info/{room_id}")
+    config.add_route("get_user_rooms", "/rooms/list/{username}")
+
+    # Room auth routes
+    config.add_route("create_room", "/rooms/create")
+    config.add_route("change_host", "/rooms/{room_id}/host")
+    config.add_route("join_room", "/rooms/{room_id}/members/join")
+    config.add_route("leave_room", "/rooms/{room_id}/members/leave")
--- video_server/views/default.py
+++ video_server/views/default.py
@@ -5,10 +5,141 @@ from sqlalchemy.exc import DBAPIError
 
 from .. import models
 
+# User public views
+@view_config(
+    route_name="users", request_method="GET", renderer="json", permission="public"
+)
+def get_all_users(request):
+    """Retrieves all registered users"""
+    pass
 
-@view_config(route_name="home", renderer="json")
-def my_view(request):
-    return {"message": "This application is running."}
+
+@view_config(
+    route_name="user", request_method="GET", renderer="json", permission="public"
+)
+def get_user_by_username(request):
+    """Retrieves a user by its username"""
+    pass
+
+
+@view_config(
+    route_name="create_user",
+    request_method="POST",
+    renderer="json",
+    permission="public",
+)
+def create_user(request):
+    """Creates a new user and authenticates session"""
+    pass
+
+
+@view_config(
+    route_name="authenticate",
+    request_method="POST",
+    renderer="json",
+    permission="public",
+)
+def authenticate_session(request):
+    """Authenticates the user by checking the username-password combination"""
+    pass
+
+
+# User auth views
+@view_config(
+    route_name="change_password",
+    request_method="PUT",
+    renderer="json",
+    permission="auth",
+)
+def change_password(request):
+    """Changes an authenticated user's password"""
+    pass
+
+
+@view_config(
+    route_name="change_mobile_token",
+    request_method="PUT",
+    renderer="json",
+    permission="auth",
+)
+def change_mobile_token(request):
+    """Changes an authenticated user's mobile token"""
+    pass
+
+
+@view_config(
+    route_name="delete_user",
+    request_method="DELETE",
+    renderer="json",
+    permission="auth",
+)
+def delete_user(request):
+    """Deletes an authenticated user's account"""
+    pass
+
+
+# Room public views
+@view_config(
+    route_name="get_room_info",
+    request_method="GET",
+    renderer="json",
+    permission="public",
+)
+def get_room_info(request):
+    """
+    Retrieves information about a room:
+        - room name
+        - host name
+        - list of members
+    """
+    pass
+
+
+@view_config(
+    route_name="get_user_rooms",
+    request_method="GET",
+    renderer="json",
+    permission="public",
+)
+def get_rooms_by_username(request):
+    """Retrieves a list of rooms a user is in"""
+    pass
+
+
+# Room auth views
+@view_config(
+    route_name="create_room", request_method="POST", renderer="json", permission="auth",
+)
+def create_room(request):
+    """Creates a room for an authenticated user and set user as the host"""
+    pass
+
+
+@view_config(
+    route_name="change_host", request_method="PUT", renderer="json", permission="auth",
+)
+def change_host(request):
+    """Changes the room host. Current user must be a host"""
+    pass
+
+
+@view_config(
+    route_name="join_room", request_method="POST", renderer="json", permission="auth",
+)
+def join_room(request):
+    """Enables the user to join a room if still within room capacity"""
+    pass
+
+
+@view_config(
+    route_name="leave_room",
+    request_method="DELETE",
+    renderer="json",
+    permission="auth",
+)
+def leave_room(request):
+    """Removes the user from the room"""
+    pass
 
 
 db_err_msg = """\
