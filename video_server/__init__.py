d867644d460e711f4647f7b50b5a9e0a2b280d74
jgeneaguilar <jgeneaguilar@gmail.com>
2020-07-09 20:03:48 +0800

    Add development env

--- video_server/__init__.py
+++ video_server/__init__.py
@@ -1,12 +1,15 @@
+import os
 from pyramid.config import Configurator
 
 
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
+    """ This function returns a Pyramid WSGI application."""
+
+    # Get
+    settings["sqlalchemy.url"] = os.environ["DATABASE_URL"]
     with Configurator(settings=settings) as config:
         config.include(".models")
-        # config.include("pyramid_jinja2")
+        config.include("pyramid_jinja2")
         config.include(".routes")
         config.include(".security")
         config.scan()
